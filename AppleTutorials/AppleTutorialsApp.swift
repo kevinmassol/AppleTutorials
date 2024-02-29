dcb46d13cbcdf4a25f356a7ed1370ce76ba651b8
Kevin Massol <kevin.massol@icloud.com>
2024-02-29 15:14:51 +0100

    Initial Commit

---
+++ AppleTutorials/AppleTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialsApp.swift
+//  AppleTutorials
+//
+//  Created by KEVIN on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
