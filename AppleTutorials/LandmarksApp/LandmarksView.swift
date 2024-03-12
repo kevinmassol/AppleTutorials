004a94e2a4e49e02b8909115fda43b7bbd7957c9
Kevin Massol <kevin.massol@icloud.com>
2024-03-12 15:37:17 +0100

    [FEAT #2](LandmarksApp) - Create LandmarksView

--- AppleTutorials/LandmarksApp/LandmarksView.swift
+++ AppleTutorials/LandmarksApp/LandmarksView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct LandmarksView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                Spacer()
+                Text("California")
+            }
+            .font(.subheadline)
+        }
+        .padding()
     }
 }
 
